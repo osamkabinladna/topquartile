7105e01085904fe49eca50dbcc215d9fea31fde0
Greg Ruyoga <gregruyoga@proton.me>
2025-05-24 15:54:30 +0200

    Evaluation -> partition_data completed (works)

---
+++ topquartile/modules/evaluation.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+
+class Evaluation():
+    def __init__(self, df: pd.DataFrame,  n_train: int = 252, n_valid: int = 1):
+        self.n_train = n_train
+        self.n_valid = n_valid
+        self.df = df
+        self.results = []
+
+        self.df.sort_index()
+
+    def partition_data(self):
+        results = []
+
+        all_unique_dates = self.df.index.get_level_values('DateIndex').unique().sort_values()
+
+        if len(all_unique_dates) < self.n_train + self.n_valid:
+            print(
+                f"Not enough unique dates ({len(all_unique_dates)}) to form even one train/test split with train_length={self.n_train} and test_length={self.n_valid}.")
+        else:
+            for i in range(self.n_train, len(all_unique_dates) - self.n_valid + 1):
+
+                train_period_end_date = all_unique_dates[i - 1]
+                train_period_start_date = all_unique_dates[i - self.n_train]
+
+                test_period_start_date = all_unique_dates[i]
+                test_period_end_date = all_unique_dates[i + self.n_valid - 1]
+
+
+                current_train_candidate = self.df.loc[pd.IndexSlice[:, train_period_start_date:train_period_end_date], :]
+                current_test_candidate = self.df.loc[pd.IndexSlice[:, test_period_start_date:test_period_end_date], :]
+
+
+                if current_train_candidate.empty or current_test_candidate.empty:
+                    continue
+
+                train_counts = current_train_candidate.groupby(level='TickerIndex').size()
+                valid_train_tickers = train_counts[train_counts == self.n_train].index
+
+                test_counts = current_test_candidate.groupby(level='TickerIndex').size()
+                valid_test_tickers = test_counts[test_counts == self.n_valid].index
+
+                common_valid_tickers = valid_train_tickers.intersection(valid_test_tickers)
+
+                if not common_valid_tickers.empty:
+                    final_train_df = current_train_candidate.loc[pd.IndexSlice[common_valid_tickers, :], :]
+                    final_test_df = current_test_candidate.loc[pd.IndexSlice[common_valid_tickers, :], :]
+
+                    # Double CHECK
+                    if not final_train_df.empty and not final_test_df.empty:
+                        results.append((final_train_df, final_test_df))
+
+        return results
