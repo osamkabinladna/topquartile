8579b6fad8b8895766b2eba610739b38278898ee
s-taha0609 <sintaroutahara@gmail.com>
2025-05-25 12:47:11 +0200

    add some variables

--- topquartile/experiments/optimize/qrf_v2.py
+++ topquartile/experiments/optimize/qrf_v2.py
@@ -15,3 +15,13 @@ covariate_dict = dict(ema = [10,20,30],
                       sma = [10,20,30],
                       volatility = [10,20,30])
 
+
+covariate_config = [(TechnicalCovariateTransform, covariate_dict)]
+label_dict = dict(label_duration = 30, quantile = 0.75])
+label_config = [(BinaryLabelTransform, label_dict)]
+partition_dict = dict(n_splits = 5, max_train_size=10,test_size= 60,
+                      gap = 20,)
+
+dataloader = DataLoader(data_id='dec2024', covariate_transform = covariate_config,
+                        label_transform=label_config, partition_class = PurgedTimeSeriesPartition,
+                        partition_kwargs=partition_dict)
