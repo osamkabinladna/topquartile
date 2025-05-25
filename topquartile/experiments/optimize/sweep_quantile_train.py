c24d087abad2c75ef998ee682a2694fcb38092dd
田原慎太郎 <shintarou@staha.local>
2025-05-25 11:02:55 +0200

    dataloader

--- topquartile/experiments/optimize/sweep_quantile_train.py
+++ topquartile/experiments/optimize/sweep_quantile_train.py
@@ -1,8 +1,12 @@
-import optuna
+import wandb
 from quantile_forest import RandomForestQuantileRegressor
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
+from topquartile.modules.datamodule.dataloader import DataLoader
+from topquartile.modules.datamodule.transforms.covariate import (TechnicalCovariateTransform, FundamentalCovariateTransform)
+from topquartile.modules.datamodule.transforms.label import BinaryLabelTransform
+from topquartile.modules.datamodule.partitions import PurgedTimeSeriesPartition
 
 
 def objective_rmse_only(trial):
@@ -12,6 +16,20 @@ def objective_rmse_only(trial):
         'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 10),
         'max_features': trial.suggest_float('max_features', 0.3, 1.0)
     }
+
+    covtrans_config = [((TechnicalCovariateTransform, dict(sma = [20, 30],
+                                                       ema = [20, 30],
+                                                       momentum_change=True,
+                                                       volatility = [20, 30],)))]
+
+    labeltrans_config = [(BinaryLabelTransform, dict(label_duration=20,
+                                                quantile=0.75))]
+
+    partition_config = dict(n_splits=5, gap=20, max_train_size=504, test_size=60, verbose=False)
+
+    dataloader = DataLoader(data_id='dec2024', covariate_transform=covtrans_config,
+                  label_transform=labeltrans_config, partition_class=PurgedTimeSeriesPartition,
+                  partition_kwargs=partition_config)
 
     wandb.init(project="quantile-sweep-rmse", config=config)
     rmse_scores = []
@@ -35,7 +53,7 @@ def objective_rmse_only(trial):
         )
 
         model.fit(X_train.values, y_train.values)
-        y_pred = model.predict(X_valid.values, quantiles=[0.5])  # 中央のみ使う
+        y_pred = model.predict(X_valid.values, quantiles=[0.5])  
         q50 = y_pred[:, 0]
 
         rmse = np.sqrt(mean_squared_error(y_valid, q50))
