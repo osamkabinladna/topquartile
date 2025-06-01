d56161fa5bfa9736bebe9d881902f3a17462e54d
Greg Ruyoga <gregruyoga@proton.me>
2025-06-01 09:32:05 +0200

    Revert to forward filling quarterly features

--- topquartile/modules/datamodule/dataloader.py
+++ topquartile/modules/datamodule/dataloader.py
@@ -167,10 +167,10 @@ class DataLoader:
                 self.data = transformer.transform()
 
             self.data.index = self.data.index.set_names(['ticker', 'Dates'])
-            # self._ffill_covariates()
+            self._ffill_covariates()
             # self._fill_dividends()
-            # self.data = self.data.replace('#NAME?', np.nan)
-            # self.data = self.data.apply(pd.to_numeric, errors='ignore')
+            self.data = self.data.replace('#NAME?', np.nan)
+            self.data = self.data.apply(pd.to_numeric, errors='ignore')
 
         return self.data
 
